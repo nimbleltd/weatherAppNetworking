6d512eaade0cd8ff957ecdb50df8e992f2b83dde
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-24 17:22:03 -0500

    current temp now in the view, closes #4

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let icon = WeatherIcon.PartlyCloudyDay.image
+        let currentWeather = CurrentWeather(tempurature: 59.0, humidity: 0.6, precipitationProbability: 0.9, summary: "Wet and rainy!", icon: icon)
+        
+        display(currentWeather)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +34,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func display(weather: CurrentWeather) {
+        currentTemperatureLabel.text = "\(weather.tempurature)"
+    }
 
 }
 
