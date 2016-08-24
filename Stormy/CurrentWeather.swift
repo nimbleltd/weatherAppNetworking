5db7fa0274724cb5ff90c4459001f9bd3515b884
Paul Campbell <paulcampbell@pcampbellHLlaptop.local>
2016-08-24 16:56:36 -0500

    create CurrentWeather model file and constant variables, closes #2

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Paul Campbell on 8/24/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let tempurature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: UIImage
+}
